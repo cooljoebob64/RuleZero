807f575263340b2d1934a113db49b9f6ed185328
kirkforaday <wrckirk@gmail.com>
2021-03-16 10:49:31 -0500

    Retool Screens With Functioning UI

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
-  
 import React from 'react';
-import Providers from './src/navigation/index';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation;
 
-export default function App() {
-  return <Providers />;
+export default class App extends React.Component {
+   render() {
+      return (
+         <View style = {styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Changes you make will automatically reload.</Text>
+            <Text>Shake your phone to open the developer menu.</Text>
+         </View>
+      );
+   }
 }
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
+});
